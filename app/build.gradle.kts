752f48eadde69198a2ae9d84000c166d2e3f1921
RyoTanabe <rtanabe@asx.co.jp>
2025-01-16 09:52:03 +0900

    Add Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("com.squareUp.retrofi2:retrofit:2.9.0")//Retrofit2ライブラリ用
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")//Retrofitスカラーコンバータ用
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
