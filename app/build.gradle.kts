a18209c353273ddc297725d02f4a042c700bc851
RyoTanabe <rtanabe@asx.co.jp>
2025-01-19 12:12:23 +0900

    Display downloaded images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     // Kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ plugins {
     id("com.android.library") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+
 }
 
 tasks.register("clean", Delete::class) {
